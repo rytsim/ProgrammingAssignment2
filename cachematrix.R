ffa9dcc48331d7edff0e1682cf2268aef4d4e3a4
Rytis Simanaitis <rytis@DOVRE.LAN>
2016-02-04 13:13:08 +0200

    two functions for the assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix - creates a list of function to get, set, get_inverse and set_inverse matrixes
+## cacheSolve - checks if there is a inverse matrix cached if not calculates the inverse
 
-## Write a short comment describing this function
+## makeCacheMatrix - creates a list of function to get, set, get_inverse and set_inverse matrixes
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse_x <- NULL
+    #get set data methods
+    set <- function(y) {
+        x <<- y
+        inverse_x <- NULL
+    }
+    get <- function() x
+    
+    #get set inverse matrix methods
+    set_inverse <- function(inverse) inverse_x <<- inverse
+    get_inverse <- function() inverse_x
+    list(set = set, get = get,
+         set_inverse = set_inverse,
+         get_inverse = get_inverse)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve - checks if there is a inverse matrix cached if not calculates the inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inverse_x <- x$get_inverse()
+    if(!is.null(inverse_x)) {
+        message("getting cached data")
+        return(inverse_x)
+    }
+    data <- x$get()
+    inverse_x <- solve(data, ...)
+    x$set_inverse(inverse_x)
+    inverse_x
 }
